d05082a6596a1de9a9a611a530946d57dac3091c
devffery <mails4icy@gmail.com>
2024-06-18 20:55:50 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './core/templates/core/**/*.html',   // Matches all HTML files in core/templates/core directory
+    './core/templates/core/**/*.css',    // Matches all CSS files in core/templates/core directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
